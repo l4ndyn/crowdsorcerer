4bda0be2ba538f436b985af27f80c755bef6e802
l4ndyn <l4ndyn@gmail.com>
2022-02-03 19:26:36 +0200

    add dummy voice message, yt/spotify url recognition

---
+++ Crowdsorcerer/VoiceMessageModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace Crowdsorcerer
+{
+    record VoiceMessage
+    {
+        public string audioUrl, description;
+    }
+
+    public class VoiceMessageModule : NancyModule
+    {
+        public VoiceMessageModule()
+        {
+            Post("/voiceMessages", x =>
+            {
+                VoiceMessage body = this.Bind();
+                Console.WriteLine(body);
+
+                return HttpStatusCode.OK;
+            });
+        }
+    }
+}
