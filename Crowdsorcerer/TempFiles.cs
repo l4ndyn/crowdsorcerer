3c47a5bbdcd2525d8bafbc1326ee550369a0d609
l4ndyn <l4ndyn@gmail.com>
2022-03-08 00:12:51 +0200

    add unique file name api

---
+++ Crowdsorcerer/TempFiles.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Crowdsorcerer
+{
+    public static class TempFiles
+    {
+        static readonly HashSet<string> tempFiles = new();
+
+        public static string New()
+        {
+            var path = Path.GetTempFileName();
+            tempFiles.Add(path);
+            
+            return path;
+        }
+
+        public static void Clear()
+        {
+            foreach (var tempFile in tempFiles)
+            {
+                try
+                {
+                    File.Delete(tempFile);
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            }
+
+            tempFiles.Clear();
+        }
+
+        public static void PrintAll()
+        {
+            foreach (var tempFile in tempFiles)
+                Console.WriteLine(tempFile);
+        }
+    }
+}
---
+++ Crowdsorcerer/UniqueFileNames.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Crowdsorcerer
+{
+    public static class UniqueFileNames
+    {
+        public static string New(string extension) => Guid.NewGuid() + "." + extension;
+    }
+}
