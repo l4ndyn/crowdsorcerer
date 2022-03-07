eb3e893c5bea180f83a75137f16ee1d91c001f7c
l4ndyn <l4ndyn@gmail.com>
2022-03-08 00:15:11 +0200

    add test code

--- Crowdsorcerer/Program.cs
+++ Crowdsorcerer/Program.cs
@@ -1,20 +1,35 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Net;
-using System.Net.Http;
-using Crowdsorcerer;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Crowdsorcerer.Projector;
+using Crowdsorcerer.Youtube;
 using Nancy;
 using Nancy.Hosting.Self;
 
 const string ENDPOINT = "http://localhost:12234/";
 
-Sorcerer.Init();
+//Sorcerer.Init();
 
-using (var nancyHost = new NancyHost(new Uri(ENDPOINT)))
+var stopwatch = Stopwatch.StartNew();
+//var path = await new YoutubeProviderLibvideoFast().Get("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+//var uris = await new YoutubeProviderLibvideoFast().GetUris("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+var uris = await new YoutubeProviderLibvideoFast().GetUris("https://www.youtube.com/watch?v=DvKKziPTB7w");
+stopwatch.Stop();
+
+var uri = VideoMerger.GetUniqueOutputUri();
+VideoMerger.Merge(uris.audioUri, uris.videoUri, uri);
+
+//VideoWindow videoWindow = new(path);
+VideoWindow videoWindow = new();
+videoWindow.Shown += (_, _) => videoWindow.Play(uri);
+Application.Run(videoWindow);
+
+/*using (var nancyHost = new NancyHost(new Uri(ENDPOINT)))
 {
     nancyHost.Start();
 
     Console.WriteLine($"Listening on {ENDPOINT}. Press enter to stop.");
 
     Console.ReadKey();
-}
+}*/
