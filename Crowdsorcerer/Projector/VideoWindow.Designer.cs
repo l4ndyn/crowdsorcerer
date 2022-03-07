7414a9320284170473e8672a0b1c62e7bef210e2
l4ndyn <l4ndyn@gmail.com>
2022-03-08 00:13:03 +0200

    add video projector

---
+++ Crowdsorcerer/Projector/VideoWindow.Designer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crowdsorcerer.Projector
+{
+    public partial class VideoWindow : Form
+    {
+        private LibVLCSharp.WinForms.VideoView videoView;
+
+        private void InitializeComponent()
+        {
+            this.videoView = new LibVLCSharp.WinForms.VideoView();
+            this.audioView = new LibVLCSharp.WinForms.VideoView();
+            ((System.ComponentModel.ISupportInitialize)(this.videoView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.audioView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // videoView
+            // 
+            this.videoView.BackColor = System.Drawing.Color.Black;
+            this.videoView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.videoView.Location = new System.Drawing.Point(0, 0);
+            this.videoView.MediaPlayer = null;
+            this.videoView.Name = "videoView";
+            this.videoView.Size = new System.Drawing.Size(284, 261);
+            this.videoView.TabIndex = 0;
+            this.videoView.Text = "videoView1";
+            // 
+            // audioView
+            // 
+            this.audioView.BackColor = System.Drawing.Color.Black;
+            this.audioView.Location = new System.Drawing.Point(0, 0);
+            this.audioView.MediaPlayer = null;
+            this.audioView.Name = "audioView";
+            this.audioView.Size = new System.Drawing.Size(75, 23);
+            this.audioView.TabIndex = 1;
+            this.audioView.Text = "videoView1";
+            this.audioView.Visible = false;
+            // 
+            // VideoWindow
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.audioView);
+            this.Controls.Add(this.videoView);
+            this.Name = "VideoWindow";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.VideoWindow_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.videoView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.audioView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private LibVLCSharp.WinForms.VideoView audioView;
+        private System.ComponentModel.IContainer components;
+    }
+}
---
+++ Crowdsorcerer/Projector/VideoWindow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+using LibVLCSharp.Shared;
+
+namespace Crowdsorcerer.Projector
+{
+    public partial class VideoWindow : Form
+    {
+        LibVLC libVLC;
+        MediaPlayer videoPlayer;
+        //MediaTimelineController
+
+        public VideoWindow()
+        {
+            if (!DesignMode)
+            {
+                Core.Initialize();
+            }
+
+            InitializeComponent();
+
+            libVLC = new LibVLC();
+            videoPlayer = new MediaPlayer(libVLC);
+
+            videoView.MediaPlayer = videoPlayer;
+        }
+
+        public void Play(string videoPath) => PlayOn(videoPlayer, new(libVLC, videoPath));
+        public void Play(Uri videoUri) => PlayOn(videoPlayer, new(libVLC, videoUri));
+
+        void PlayOn(MediaPlayer player, Media media)
+        {
+            player.Play(media);
+            media.Dispose();
+        }
+
+        private void VideoWindow_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            videoPlayer.Stop();
+            videoPlayer.Dispose();
+            libVLC.Dispose();
+        }
+    }
+}
