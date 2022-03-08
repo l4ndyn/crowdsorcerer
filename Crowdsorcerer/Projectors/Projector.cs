9610c499453f943c0475e114e908c9b35a3e1f00
l4ndyn <l4ndyn@gmail.com>
2022-03-08 05:02:18 +0200

    add projector

---
+++ Crowdsorcerer/Projectors/Projector.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Crowdsorcerer.Youtube;
+
+namespace Crowdsorcerer.Projectors
+{
+    public class Projector
+    {
+        VideoWindow videoWindow;
+        YoutubeProviderLibvideoFast youtubeProvider;
+
+        public Projector()
+        {
+            youtubeProvider = new();
+
+            Task.Run(() =>
+            {
+                videoWindow = new();
+
+                //videoWindow.Shown += (_, _) => ProjectYoutube("https://www.youtube.com/watch?v=ZmxF0vxpXa0&ab_channel=JeaneyCollects");
+                //videoWindow.VideoFinished += () =>
+                //    ProjectYoutube("https://www.youtube.com/watch?v=Ql2zKS_bdfE&ab_channel=CaptainMayo");
+                Application.Run(videoWindow);
+            });
+        }
+
+        public void ProjectYoutube(string url)
+        {
+            Task.Run(async () =>
+            {
+                var (audioUri, videoUri) = await youtubeProvider.GetUris(url);
+
+                //ProjectMuxed(videoUri, audioUri);
+                ProjectSynced(videoUri, audioUri);
+
+            });
+
+            //doesnt neccessarily work
+            void ProjectMuxed(Uri videoUri, Uri audioUri)
+            {
+                var outputStreamUri = VideoMerger.GetUniqueOutputUri();
+                VideoMerger.Merge(audioUri, videoUri, outputStreamUri);
+
+                videoWindow.Play(outputStreamUri);
+            }
+
+            void ProjectSynced(Uri videoUri, Uri audioUri)
+            {
+                videoWindow.Play(videoUri, audioUri);
+            }
+        }
+    }
+}
--- Crowdsorcerer/Projectors/VideoWindow.cs
+++ Crowdsorcerer/Projectors/VideoWindow.cs
@@ -2,13 +2,14 @@
 using System.Windows.Forms;
 using LibVLCSharp.Shared;
 
-namespace Crowdsorcerer.Projector
+namespace Crowdsorcerer.Projectors
 {
     public partial class VideoWindow : Form
     {
         LibVLC libVLC;
         MediaPlayer videoPlayer;
-        //MediaTimelineController
+
+        public event Action VideoFinished;
 
         public VideoWindow()
         {
@@ -23,15 +24,33 @@ namespace Crowdsorcerer.Projector
             videoPlayer = new MediaPlayer(libVLC);
 
             videoView.MediaPlayer = videoPlayer;
+
+            videoPlayer.EndReached += (_, _) => VideoFinished?.Invoke();
         }
 
         public void Play(string videoPath) => PlayOn(videoPlayer, new(libVLC, videoPath));
         public void Play(Uri videoUri) => PlayOn(videoPlayer, new(libVLC, videoUri));
+        public void Play(Uri videoUri, Uri audioUri) => PlayOn(videoPlayer, new(libVLC, videoUri), audioUri.ToString());
 
         void PlayOn(MediaPlayer player, Media media)
         {
-            player.Play(media);
+            player.Stop();
+
+            player.Media = media;
+            player.Play();
+
             media.Dispose();
+        }
+        void PlayOn(MediaPlayer player, Media video, string audioUri)
+        {
+            player.Stop();
+
+            player.Media = video;
+            player.AddSlave(MediaSlaveType.Audio, audioUri, false);
+
+            player.Play();
+
+            video.Dispose();
         }
 
         private void VideoWindow_FormClosed(object sender, FormClosedEventArgs e)
