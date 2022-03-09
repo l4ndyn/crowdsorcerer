5cb2586c1c27780689183ad2349c95699a331c38
l4ndyn <l4ndyn@gmail.com>
2022-03-09 02:19:38 +0200

    stop unneccessary ffmpeg processes

--- Crowdsorcerer/Youtube/VideoMerger.cs
+++ Crowdsorcerer/Youtube/VideoMerger.cs
@@ -13,7 +13,8 @@ namespace Crowdsorcerer.Youtube
         static string FFmpegPath => Path.Combine(Environment.CurrentDirectory, "FFmpeg\\bin\\x64\\FFmpeg.exe");
 
         static CancellationTokenSource cts = new();
-        
+        static Uri previousOutputUri;
+
         public static async Task Merge(string sourceAudioPath, string sourceVideoPath, string outputPath)
         {
             var res = await Cli.Wrap(FFmpegPath)
@@ -24,20 +25,40 @@ namespace Crowdsorcerer.Youtube
 
             Console.WriteLine("Output file successfully created.");
         }
+
+        //TODO: could be unsafe to return the output uri without lock
         public static async Task Merge(Uri sourceAudioUri, Uri sourceVideoUri, Uri outputUri)
-        {
+        { 
             cts.Cancel();
+            if (previousOutputUri != null) ReturnOutputUri(previousOutputUri);
+
             cts = new();
+            previousOutputUri = outputUri;
 
             var res = await Cli.Wrap(FFmpegPath)
-                .WithArguments($"-i {sourceVideoUri} -i {sourceAudioUri} -c:v h264 -c:a aac -f flv -listen 1 {outputUri}")
-                .ExecuteBufferedAsync(cts.Token);
+                .WithArguments($"-i \"{sourceVideoUri}\" -i \"{sourceAudioUri}\" -c:v h264 -c:a aac -f flv -listen 1 {outputUri}")
+                //.WithStandardErrorPipe(PipeTarget.ToDelegate(l => Console.WriteLine("[ffmpeg] " + l)))
+                .ExecuteBufferedAsync();
 
-            Console.WriteLine(res.StandardOutput);
+            //ReturnOutputUri(outputUri);
+            //Console.WriteLine(res.StandardOutput);
 
             Console.WriteLine("Output stream successfully created.");
         }
 
-        public static Uri GetUniqueOutputUri() => new("http://localhost:12235/crowdsorcerer/" + Unique.Name());
+        public static Uri GetUniqueOutputUri() => new($"http://localhost:{Unique.Port()}/crowdsorcerer/video");
+
+        static void ReturnOutputUri(Uri uri)
+        {
+            string uriString = uri.ToString();
+
+            int colon = uriString.LastIndexOf(':');
+            string startingAtPort = uriString.Substring(colon + 1);
+
+            int slash = startingAtPort.IndexOf('/');
+            int port = int.Parse(startingAtPort.Substring(0, slash));
+
+            Unique.ReturnPort(port);
+        }
     }
 }
